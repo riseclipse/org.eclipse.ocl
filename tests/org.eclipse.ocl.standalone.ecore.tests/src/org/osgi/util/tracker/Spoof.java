427f8c8ee0a75ed8b6389c032b0881bce6dbc870
Ed Willink <ed@willink.me.uk>
2025-10-31 20:31:03 +0000

    [#2381] Add org.osgi.util.tracker.Spoof for EMF 2.43

---
+++ tests/org.eclipse.ocl.standalone.ecore.tests/src/org/osgi/util/tracker/Spoof.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Willink Transformations and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *     E.D.Willink - Initial API and implementation
+ *******************************************************************************/
+
+package org.osgi.util.tracker;
+
+/**
+ * This class provides spoof content to enable a spoof org.osgi.util.tracker package to exist for
+ * non-OSGI testing, thereby working around Bug 535763
+ * @author ed
+ *
+ */
+public class Spoof
+{
+}
