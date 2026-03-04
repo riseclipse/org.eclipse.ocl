fa1c03ee716a7dc42411c543eae19df2b0b2c78a
ilpleutdehors <yannsullychesnelbicep@laposte.net>
2026-03-04 14:46:27 +0100

    feat: Generate all the functions.

---
+++ Store/src/store/impl/StoreFactoryImpl.java
@@ -0,0 +1,128 @@
+/**
+ */
+package store.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+import store.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class StoreFactoryImpl extends EFactoryImpl implements StoreFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static StoreFactory init() {
+		try {
+			StoreFactory theStoreFactory = (StoreFactory)EPackage.Registry.INSTANCE.getEFactory(StorePackage.eNS_URI);
+			if (theStoreFactory != null) {
+				return theStoreFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new StoreFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public StoreFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case StorePackage.STORE: return createStore();
+			case StorePackage.BOOK: return createBook();
+			case StorePackage.CD: return createCD();
+			case StorePackage.PERSON: return createPerson();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Store createStore() {
+		StoreImpl store = new StoreImpl();
+		return store;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Book createBook() {
+		BookImpl book = new BookImpl();
+		return book;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public CD createCD() {
+		CDImpl cd = new CDImpl();
+		return cd;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public Person createPerson() {
+		PersonImpl person = new PersonImpl();
+		return person;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public StorePackage getStorePackage() {
+		return (StorePackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static StorePackage getPackage() {
+		return StorePackage.eINSTANCE;
+	}
+
+} //StoreFactoryImpl
